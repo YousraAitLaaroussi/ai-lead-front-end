b858dd395e09e2373135bce152c1e7dde4c8804b
YousraAitLaaroussi <Aitlaarousi16@gmail.com>
2025-07-22 16:19:10 +0100

    creation de LayoutPublic file

---
+++ ai-lead/src/components/NavPublic.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <h1>Bienvenue</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'NavPublic'
+}
+</script>
---
+++ ai-lead/src/components/footerPublic.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <p>© 2025 Tous droits réservés</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'footerPublic'
+}
+</script>
