1a03e668be377ad9e35ac8fd80e99b0043085a3e
douaebh <douaaebh18@gmail.com>
2025-07-29 18:42:30 +0100

    call monior

--- ai-lead/src/router/index.js
+++ ai-lead/src/router/index.js
@@ -72,7 +72,7 @@ const routes = [
     path: "/Client/billing",
     name: "billing",
     component: () => import("@/pages/Client/billing-plan.vue"),
-  }
+  },
 
   {
    path: "/campaigne",
@@ -90,6 +90,11 @@ const routes = [
    path: "/clientmanagment",
     name: "client",
     component: () => import("@/pages/Admin/ClientManagment.vue"),
+  }, 
+  {
+   path: "/CallMonitor",
+    name: "call",
+    component: () => import("@/pages/Admin/CallMonitor.vue"),
   },
 ]
 
